bb64158a8af9a4dcf8ee85e564323dda946790c8
huilinghe19 <huiling.he@helmholtz-berlin.de>
2020-12-14 11:46:15 +0100

    copy the goib_test to a destination and change gpib address in st.cd

---
+++ Website_epicsIOC_flask/find.py
@@ -0,0 +1,36 @@
+                            
+import os
+import shutil
+def search_file(path,str):
+    for x in os.listdir(path):
+        next_path=os.path.join(path,x)
+        if os.path.isfile(next_path):
+            if str in x:
+                print(os.path.relpath(next_path, p))
+        else:
+            search_file(next_path,str)
+#p=input('input search path：')
+#str=input('input search str of the file name：')
+#search_file(p,str)
+def copyTemplates(src, dst):
+    shutil.copytree(src, dst)
+
+
+
+
+def findContentFile():
+    with open("/home/pi/copyTemplatesDestination/gpib_test/iocBoot/iocgpib/st.cmd", "r", encoding="utf-8") as f:
+        lines = f.readlines()
+
+    with open("/home/pi/copyTemplatesDestination/gpib_test/iocBoot/iocgpib/st.cmd", "w", encoding="utf-8") as f_w:
+        for line in lines:
+            if "ADDR=19" in line:
+                line=line.replace(line, 'dbLoadRecords "db/yourdev.db", "P=${IOC}:,PORT=$(Port),ADDR=20" ')
+            
+            f_w.write(line)
+
+if __name__== '__main__':
+    copyTemplates("Templates/Keithley/keithley2000", "copyTemplatesDestination")
+    findContentFile()
+
+
