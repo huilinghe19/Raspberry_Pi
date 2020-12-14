6d0e37cb9ddd29cde7971e1f0c9467deb068d8a3
huilinghe19 <huiling.he@helmholtz-berlin.de>
2020-12-14 11:54:53 +0100

    templates for all gpib format added

--- Epics_Installation/gpib_test/gpib_test/iocBoot/iocgpib/st.cmd
+++ Epics_Installation/gpib_test/gpib_test/iocBoot/iocgpib/st.cmd
@@ -7,22 +7,43 @@
 epicsEnvSet(STREAM_PROTOCOL_PATH, "../../gpibApp/Db")
 cd "${TOP}"
 
-## Register all support components
+## 1  Register all support components
 dbLoadDatabase("dbd/gpib.dbd")
 gpib_registerRecordDeviceDriver(pdbbase)
+
+
+## 2 Configure your asynPorts
+# 2a GPIB: portname, autoConnect, boardId, timeout, 5th_arg
+     # Note1: The portname has to match the interface name of your board
+     #        from /etc/gpib.conf!
+     # Note2: boardId is the GPIB address *of your GPIB controller board*.
+     #        The GPIB address of the _device_ itself is supplied to
+     #        dbLoadRecords under step 3 below.
 
 epicsEnvSet("Port", "raspi_gpio_interface")
 
 GpibBoardDriverConfig("$(Port)", "1", "0", "3", "0")
-## Load record instances
-#dbLoadTemplate "db/Keithley2000.substitutions"
+
+
+# 2b TCP/IP socket: IPaddress:port
+#drvAsynIPPortConfigure("IPPORT","192.168.0.1:6789")
+
+# 2c Serial: "COM1", 9600 bps, 8N1, flow contr.: XON/XOFF
+#drvAsynSerialPortConfigure ("SERIALPORT", "/dev/ttyS0", 0, 0, 0)
+#asynSetOption ("SERIALPORT", 0, "baud", "9600")
+#asynSetOption ("SERIALPORT", 0, "bits", "8")
+#asynSetOption ("SERIALPORT", 0, "parity", "none")
+#asynSetOption ("SERIALPORT", 0, "stop", "1")
+#asynSetOption ("SERIALPORT", 0, "clocal", "Y")
+#asynSetOption ("SERIALPORT", 0, "crtscts", "N")
+
+# 2d vxi11: ...
+
+
+## 3 Load record instances
+# Make sure you have your device's GPIB address right!
+#dbLoadTemplate "db/Keithley6485.substitutions"
 dbLoadRecords "db/yourdev.db", "P=${IOC}:,PORT=$(Port),ADDR=19"
-
-#dbLoadRecords "db/dbExample1.db", "P=${IOC}:,PORT=$(Port),ADDR=19"
-#dbLoadRecords "db/keithleyHHL.db", "user=pi, P=${IOC}:,PORT=$(Port),ADDR=19"
-#dbLoadRecords "db/dbSubExample.db", "P=${IOC}:,PORT=$(Port),ADDR=19"
-
-#dbLoadRecords "db/user.substitutions"
 
 ## Set this to see messages from mySub
 #var mySubDebug 1
