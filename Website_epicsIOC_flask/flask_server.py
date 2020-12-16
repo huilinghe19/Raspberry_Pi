d19e563f0c819e585a7cd8f133a10db1e506b32d
huilinghe19 <huiling.he@helmholtz-berlin.de>
2020-12-16 12:26:54 +0100

    address and ioc works seperately

--- Website_epicsIOC_flask/flask_server.py
+++ Website_epicsIOC_flask/flask_server.py
@@ -2,13 +2,14 @@ from flask import Flask, request, render_template
 import binascii #needed for encoding, decoding hex to ascii/ ascii to hex
 #import GPIB_Server_Functions as ServerGPIB 
 
-import os
+import os, sys, stat
 import time
 import subprocess
 from subprocess import Popen, PIPE, STDOUT
 import logging
 
 
+#SECRET_KEY = 'development'
 app = Flask(__name__)
 
 logging.basicConfig(filename="myapp.log", level=logging.INFO)
@@ -17,64 +18,79 @@ logging.basicConfig(filename="myapp.log", level=logging.INFO)
 devices_dict = {}
 reload_content = {}
 result = ''
+data = '19'
 ###
 
-### old index, no effect except showing the links informationen, will be changed in future.
-@app.route('/', methods=['GET' ,'POST'])
-def open_index():
-    global devices_dict;global reload_content;global result
-    if request.method == 'POST' :
-        if 'action' in request.form:
-            reload_content = request.form.to_dict() #cast to dict so values can be changed; request.form elements are immutable
-            handle = devices_dict[request.form['active_element']]['handle'] #get handle int from selected device
-            command = request.form['command']
-            bytes_to_read = request.form['bytes_to_read']
-            if request.form['action'] == 'send only':
-                if request.form['send_coding'] == 'hexadecimal':
-                    command = bytes.fromhex(command).decode('ascii')
-                #ServerGPIB.send_only(command,handle)
-            elif request.form['action'] == 'read only':
-                #result = ServerGPIB.read_only(handle, bytes_to_read)
-                if request.form['read_coding'] == 'hexadecimal':
-                    result = result.encoding('ascii')
-                    result = binascii.hexlify(result)
-            elif request.form['action'] == 'send and read':
-                if request.form['send_coding'] == 'hexadecimal':
-                    command = bytes.fromhex(command).decode('ascii')
-                #result = ServerGPIB.send_and_read(command, handle, bytes_to_read)
-                if request.form['read_coding'] == 'hexadecimal':
-                    result = result.encode('ascii')
-                    result = binascii.hexlify(result)
-                    result = result.decode('ascii')
-            else:
-                print("ERROR: bad 'action' value")
-            #reload_content['status_byte'] = ServerGPIB.get_ibsta()
-            #reload_content['ibsta'] = ServerGPIB.get_ibsta()
-            #reload_content['result'] = reload_content['result'].replace('\r\n','')
 
-        else:
-            #devices_dict = ServerGPIB.get_available_devices()
-            result = ''
-            print(reload_content)
-    #print(reload_content)
-    return render_template('index.html',devices=devices_dict, result=result, reload_content=reload_content)
+@app.route('/hello',methods=['post','get'])
+def hello_world():
+    return render_template('example.html',data=[{'address':'19'}, {'address':'2'}, {'address':'3'}, {'address':'4'}, {'address':'5'}, {'address':'6'}, {'address':'7'}])
+
+@app.route('/test',methods=['post','get'])
+def test():
+    select = request.form.get('comp_select')
+    return(str(select))
+
+
+
+@app.route('/',methods=['GET' ,'POST'])
+def open_index():  
+    return render_template('index.html', devices=devices_dict, result=result, reload_content=reload_content, data=[{'address':'19'}, {'address':'2'}, {'address':'3'}])
 
 ### Webpage for Keithley 2000
-@app.route('/openKeithley2000IOC', methods=['GET' ,'POST'])
-def open_keithley2000ioc():
+@app.route('/openKeithley2000IOC', methods=['GET', 'POST'])
+def openKeithley2000IOC():
+    address = request.form.get('comp_select')
+    #print(" GPIB Address {}".format(address))
+    #new_line = 'dbLoadRecords "db/yourdev.db", "P=${IOC}:,PORT=$(Port),ADDR={}"'.format(address)
+    #file = "/home/pi/copyTemplatesDestination/gpib_test/iocBoot/iocgpib/st.cmd"
+    #os.chmod(file, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR )
+    #os.chmod(file, stat.S_IXGRP)
+    #os.chmod(file, stat.S_IWOTH)
+    #findContentFile("/home/pi/copyTemplatesDestination/gpib_test/iocBoot/iocgpib/st.cmd", "ADDR=", new_line)
     if request.method == 'POST' :
-        logging.info('open keithley2000 IOC... {}'.format(time.asctime(time.localtime(time.time()))))
-        runApp()
-    return render_template('index.html', devices=devices_dict, result=result, reload_content=reload_content)
-def runApp():
+        print(request.form)
+        if 'action' in request.form:
+            if request.form['action'] == 'getAddress':
+                print("get Address {}".format(address))
+                #return render_template('keithley2000.html', data=str(address), devices=devices_dict, result=result, reload_content=reload_content)
+                #new_line = 'dbLoadRecords "db/yourdev.db", "P=${IOC}:,PORT=$(Port),ADDR={}"'.format(address)
+                #file = "/home/pi/copyTemplatesDestination/gpib_test/iocBoot/iocgpib/st.cmd"
+        #os.chmod(file, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR )
+        #os.chmod(file, stat.S_IXGRP)
+        #os.chmod(file, stat.S_IWOTH)
+        #findContentFile("/home/pi/copyTemplatesDestination/gpib_test/iocBoot/iocgpib/st.cmd", "ADDR=", new_line)
+            elif request.form['action'] == 'startIOC':
+                
+                print("start IOC")
+                runApp()
+                #return render_template('keithley2000.html', data=str(address), devices=devices_dict, result=result, reload_content=reload_content)
+    return render_template('keithley2000.html', data=str(address), devices=devices_dict, result=result, reload_content=reload_content)
+def runApp():   
+    logging.info('runApp: openKeithley2000IOC. {}'.format(time.asctime(time.localtime(time.time()))))
     command_line_args = "./st.cmd"
-    process = subprocess.run(command_line_args, shell=True, cwd="/home/pi/gpib_test/iocBoot/iocgpib")
+    process = subprocess.run(command_line_args, shell=True, cwd="/home/pi/copyTemplatesDestination/gpib_test/iocBoot/iocgpib")
     output = process.stdout
     logging.info('runApp: Keithley 2000 IOC is Started. {}'.format(time.asctime(time.localtime(time.time()))))
     #process = Popen(command_line_args, stdout=PIPE, stderr=STDOUT, cwd="/home/pi/gpib_test/iocBoot/iocgpib")
     #with process.stdout:
     #       log_subprocess_output(process.stdout)
     #exitcode = process.wait() # 0 means success
+def configureIOCfiles():
+    new_line = 'dbLoadRecords "db/yourdev.db", "P=${IOC}:,PORT=$(Port),ADDR={}"'.format(str(address))
+    print(new_Line)
+    findContentFile("/home/pi/copyTemplatesDestination/gpib_test/iocBoot/iocgpib/st.cmd", "ADDR=", new_line)
+def findContentFile(src, content_search, new_line):
+    with open(src, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+
+    with open(src, "w", encoding="utf-8") as f_w:
+        for line in lines:
+            if content_search in line:
+                line=line.replace(line, new_line)            
+            f_w.write(line)
+
+
 
 ### Webpage for Keithley 3000, just for testing other devices in future 
 @app.route('/openKeithley3000IOC', methods=['GET' ,'POST'])
