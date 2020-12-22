2049240447ba057e7c6cec16d0b89f25c299d9f6
huilinghe19 <huiling.he@helmholtz-berlin.de>
2020-12-22 10:49:30 +0100

    works, but not gut

--- Website_epicsIOC_flask/flask_server.py
+++ Website_epicsIOC_flask/flask_server.py
@@ -1,15 +1,14 @@
 
-from flask import Flask, request, render_template
-import binascii #needed for encoding, decoding hex to ascii/ ascii to hex
-#import GPIB_Server_Functions as ServerGPIB 
-
 import os, sys, stat
 import time
+import shutil
+from pathlib import Path
+from flask import Flask, request, render_template
+import binascii #needed for encoding, decoding hex to ascii/ ascii to hex
 import subprocess
 from subprocess import Popen, PIPE, STDOUT
 import logging
-import shutil
-from pathlib import Path
+
 
 #SECRET_KEY = 'development'
 app = Flask(__name__)
@@ -20,29 +19,16 @@ logging.basicConfig(filename="myapp.log", level=logging.INFO)
 devices_dict = {}
 reload_content = {}
 result = ''
-address = ""
-pvname = ""
+###
+
 SRC = "/home/pi/Templates/Keithley/keithley2000"
 DST = "/tmp/newIOC"
 address_list = [{'address':'19'}, {'address':'1'},{'address':'2'}, {'address':'3'}, {'address':'4'}, {'address':'5'}, 
 {'address':'6'}, {'address':'7'},  {'address':'8'}, {'address':'9'}, {'address':'10'}, {'address':'11'},  {'address':'12'}, {'address':'13'}, {'address':'14'},
  {'address':'15'},  {'address':'16'}, {'address':'17'}, {'address':'18'}, {'address':'20'}, {'address':'21'}, {'address':'22'},
  {'address':'23'},  {'address':'24'}, {'address':'25'}, {'address':'26'}, {'address':'27'},  {'address':'28'}, {'address':'29'}, {'address':'30'}, {'address':'31'}]
-#CONFIGURE_PARAMETERS = [{"address": ''}, {"pvname": ''}]
-###
 
-def copyTemplates(src, dst):
-    shutil.copytree(src, dst)      
-    print("src: is copied in dst: " ) 
 
-@app.route('/hello',methods=['post','get'])
-def hello_world():
-    return render_template('example.html',data=[{'address':'19'}])
-
-@app.route('/test',methods=['post','get'])
-def test():
-    select = request.form.get('comp_select')
-    return(str(select))
 
 
 
@@ -54,49 +40,66 @@ def open_index():
 ### Webpage for Keithley 2000
 @app.route('/openKeithley2000IOC', methods=['GET', 'POST'])
 def openKeithley2000IOC():
-    address = request.form.get('comp_select')
-    pvname = request.form['pv_name']
-    print(pvname)
-    print(address)
-    if request.method == 'POST':
-        print(request.form)
-        if 'action' in request.form:
-            if request.form['action'] == 'getAddress':
-                print("get Address {}".format(address))
-
-                #configure("address")
-                #configure("pvname")
-                dst = DST
-                src = SRC
-                file_path = dst + "/gpib_test/iocBoot/iocgpib/st.cmd"
-                changePermission(file_path)
-                content_search = "ADDR="
-                new_line = 'dbLoadRecords "db/yourdev.db", "P=${IOC}:,PORT=$(Port),ADDR=%s"\n' % str(address)
-                changeContent(file_path, content_search, new_line)
-
-                file2 = dst + "/gpib_test/iocBoot/iocgpib/envPaths"
-                changePermission(file2)
-                line2 = 'epicsEnvSet("IOC",%s)\n' % pvname
-                content2 = "IOC"
-                changeContent(file2, content2, line2)
-
-                file3 = dst + "/gpib_test/iocBoot/iocgpib/envPaths"
-                top_path = dst + "/gpib_test"
-                changePermission(file3)
-                line3 = 'epicsEnvSet("TOP", %s)\n' % top_path
-                content3 = "TOP"
-                changeContent(file3, content3, line3)
-                result="OK"
-                print("Configure Done")
-                
-                
-                runApp(address, dst)  
-    return render_template('keithley2000.html', data=str(address), name=str(pvname), devices=devices_dict, result=result, reload_content=reload_content)
-
-def configure(parameter):
-    print(paramater)
+    address_input = request.form.get('comp_select')
+    pvname_input = request.form['pv_name']
+    if pvname_input:
+        print("pvname: ", pvname_input)
     
-def runApp(address, dst):
+        if request.method == 'POST':
+            print(request.form)
+            if 'action' in request.form:
+                if request.form['action'] == 'getAddress':
+                    logging.info('get address {}, pv name {}, {}'.format(address_input, pvname_input, time.asctime(time.localtime(time.time()))))
+                    print("get Address {}".format(address_input))
+                    dst = DST
+                    src = SRC
+                    configure_parameters = {"address": address_input, "pvname": pvname_input}
+                    copyTemplates(src, dst)
+                    configure(configure_parameters, dst)          
+                    runApp(dst)
+                    
+    else:
+        logging.info('PV Name: NO INPUT. {}'.format(address_input, pvname_input, time.asctime(time.localtime(time.time()))))
+        return("PV Name: NO INPUT. Please give an input. ")
+
+    return render_template('keithley2000.html', data=str(address_input), name=str(pvname_input), devices=devices_dict, result=result, reload_content=reload_content)
+
+def configure(parameter_dict, dst):
+    for i in parameter_dict.keys():
+        print(i)
+        if i == "address":
+            configureAddress(parameter_dict['address'], dst)
+        elif i == "pvname":
+            configurePV(parameter_dict['pvname'], dst)
+        else:
+            pass
+            
+def configureAddress(address, dst):
+    print("configureAddress: ", address)
+ 
+    file_path = dst + "/gpib_test/iocBoot/iocgpib/st.cmd"
+    changePermission(file_path)
+    content_search = "ADDR="
+    new_line = 'dbLoadRecords "db/yourdev.db", "P=${IOC}:,PORT=$(Port),ADDR=%s"\n' % str(address)
+    changeContent(file_path, content_search, new_line)
+    
+def configurePV(pvname, dst):
+    print("configurePV: ", pvname)
+ 
+    file2 = dst + "/gpib_test/iocBoot/iocgpib/envPaths"
+    changePermission(file2)
+    line2 = 'epicsEnvSet("IOC",%s)\n' % pvname
+    content2 = "IOC"
+    changeContent(file2, content2, line2)
+
+    file3 = dst + "/gpib_test/iocBoot/iocgpib/envPaths"
+    top_path = dst + "/gpib_test"
+    changePermission(file3)
+    line3 = 'epicsEnvSet("TOP", %s)\n' % top_path
+    content3 = "TOP"
+    changeContent(file3, content3, line3)
+        
+def runApp(dst):
     print("start IOC")
     logging.info('runApp: openKeithley2000IOC. {}'.format(time.asctime(time.localtime(time.time()))))
     command_line_args = "./st.cmd"
@@ -119,6 +122,7 @@ def changeContent(src, content_search, new_line):
             f_w.write(line)
             
 def copyTemplates(src, dst):
+    shutil.rmtree(dst)
     shutil.copytree(src, dst)      
     print("src: is copied in dst: " ) 
     
@@ -128,11 +132,6 @@ def changePermission(src):
 
 
 
-
-
-
-
 if __name__ == "__main__":
-    copyTemplates(SRC, DST)
     #app.run(host='134.30.36.95', port='8080', threaded=True)
     app.run(host='192.168.1.101', port='8080', threaded=True)
